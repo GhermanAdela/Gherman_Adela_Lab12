277af447ec4b0a0b01d574f95fc931bf0a5da6db
Adela <Adela@DESKTOP-LLFS6AN>
2022-01-14 12:00:50 +0200

    Add project files.

---
+++ Gherman_AdelaIulia_Lab12/Gherman_AdelaIulia_Lab12/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gherman_AdelaIulia_Lab12.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
